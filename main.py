92eefa11f5190bb6a64aaafa04dcc4d2e29c8acd
Zari <mateusz.zarudzki@gmail.com>
2019-02-23 12:49:16 +0100

    wrong way

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 import os
 
+class Crawler:
 
-def print_identation(n):
-    i = 0
-    while i < n:
-        print('\t', end='')
-        i += 1
+    def __init__(self, root_path):
+        self.__root_path = root_path
 
+    def listDir(self):
+        return os.listdir(self.__root_path)
 
-def print_dir(dirName, lvl = 0):
-    print_identation(lvl)
-    print(dirName)
-
-
-def print_filename(filename, lvl = 0):
-    print_identation(lvl)
-    print('\t%s' % filename)
-
-
-def print_dir_tree(dirName, fileList, lvl):
-    print_dir(dirName, lvl)
-    for fname in fileList:
-        print_filename(fname, lvl)
-
+    def getRootPath(self):
+        return self.__root_path
 
 if __name__ == "__main__":
-    rootDir = '/home/mzarudzki/crawler_test'
-    lvl = 0
-    for dirName, subdirList, fileList in os.walk(rootDir,
-                                                 topdown=True,
-                                                 followlinks=False):
-        print_dir_tree(dirName, fileList, lvl)
-        lvl += 1
+    crawler = Crawler('/home/mzarudzki/crawler_test')
+    print(crawler.listDir())
 
 
