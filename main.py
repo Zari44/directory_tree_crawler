e5f8936bd9ca197a04a5f19999142cc3d88f610a
Mateusz Zarudzki <zarudzki@9livesdata.com>
2019-02-25 19:26:52 +0100

    half/working bound into a class

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ logging.basicConfig(
 )
 
 
+lock = threading.Lock()
+
+no_files = 0
+
+
+# class CrawlingThread(threading.Thread):
+#
+#     def __init__(self, thread_name):
+#         threading.Thread.__init__(self)
+#         self.thread_name = thread_name
+#
+#     def run(self):
+#         logging.warning('Starting ' + self.thread_name)
+#         crawl_tq(task_queue)
+#         logging.warning('Stopping ' + self.thread_name)
+
+
+
 class TaskQueue:
 
     def __init__(self):
@@ -37,54 +55,63 @@ class TaskQueue:
         self._running_tasks.get()
 
 
-task_queue = TaskQueue()
+class ThreadedCrawler:
 
-# dirs = queue.Queue()
-# tasks = queue.Queue()
+    def __init__(self, root_dir, no_threads):
+        self._task_queue = TaskQueue()
+        self._task_queue.put(root_dir)
+        self._threads = []
+        self.__create_thread_pool(no_threads)
 
-lock = threading.Lock()
+    def __create_thread_pool(self, no_threads):
+        logging.warning("About to create thread")
+        for i in range(1, no_threads + 1):
+            thread = threading.Thread(target=self.__crawl())
+            logging.warning("Was it started?")
+            self._threads.append(thread)
 
-no_files = 0
-
-
-class CrawlingThread(threading.Thread):
-
-    def __init__(self, thread_name):
-        threading.Thread.__init__(self)
-        self.thread_name = thread_name
-
-    def run(self):
-        logging.warning('Starting ' + self.thread_name)
-        crawl_tq(task_queue)
-        logging.warning('Stopping ' + self.thread_name)
+    def start_crawling(self):
+        for thread in self._threads:
+            thread.start()
 
 
-def crawl_tq(task_queue):
-    global no_files
+        for thread in self._threads:
+            thread.join()
 
-    while task_queue.are_running_tasks():
-        if task_queue.available_tasks():
-            root_dir = task_queue.get()
-            try:
-                print(root_dir)
-                files = os.listdir(root_dir)
-            except:
-                logging.warning("Exception!")
-            for file in files:
-                file_path_abs = os.path.join(root_dir, file)
+    def __crawl(self):
+        global no_files
 
-                if os.path.isdir(file_path_abs) and os.access(file_path_abs, os.R_OK):
-                    task_queue.put(file_path_abs)
-                elif os.path.islink(file_path_abs):
-                    # print(file)
-                    pass
-                else:
-                    # print(file)
-                    pass
+        while self._task_queue .are_running_tasks():
+            if self._task_queue .available_tasks():
+                root_dir = self._task_queue .get()
+                try:
+                    print(root_dir)
+                    files = os.listdir(root_dir)
+                except:
+                    logging.warning("Exception!")
+                for file in files:
+                    file_path_abs = os.path.join(root_dir, file)
 
-                with lock:
-                    no_files += 1
-            task_queue.task_finished()
+                    if os.path.isdir(file_path_abs) and os.access(file_path_abs, os.R_OK):
+                        self._task_queue .put(file_path_abs)
+                    elif os.path.islink(file_path_abs):
+                        # print(file)
+                        pass
+                    else:
+                        # print(file)
+                        pass
+
+                    with lock:
+                        no_files += 1
+                self._task_queue .task_finished()
+
+
+
+
+# task_queue = TaskQueue()
+
+
+
 
 
 if __name__ == "__main__":
@@ -95,20 +122,13 @@ if __name__ == "__main__":
 
     n = 5
 
-    task_queue.put(root_path)
-
-    threads = []
-    for i in range(1, n+1):
-        thread = CrawlingThread("thread"+str(i))
-        threads.append(thread)
-
     start = time.time()
 
-    for thread in threads:
-        thread.start()
 
-    for thread in threads:
-        thread.join()
+    crawler = ThreadedCrawler(root_path, n)
+
+
+    crawler.start_crawling()
 
     logging.warning("Time took: %0.2f", time.time()-start)
     logging.warning("Visited %s files ", no_files)
